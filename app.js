d7d4d8ae53378facb8247eb441948b0e4c0ef54a
rahulgupta <guptarahula546@gmail.com>
2025-09-05 01:20:40 +0530

    styling of web pages are done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js")
 const MONGO_URL = "mongodb://127.0.0.1:27017/landbnb";
 const path = require("path");
 const methodOverride= require("method-override");
+const ejsMate = require("ejs-mate");
 main()
 .then(()=>{
     console.log("connected to db");
@@ -20,6 +21,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/",(req,res)=>{
     res.send("hi i am root");
